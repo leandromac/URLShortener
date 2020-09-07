b0d6dba8cc8115d5090db58079cc48d29b486bab
leandromac <contato@leandromacedo.com>
2020-09-07 11:22:34 -0300

    Refatoring

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Information to reach API
-const apiKey = 'ee007d7eb189483ead490320df3adb86';
+const apiKey = '<api key>';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -8,20 +8,45 @@ const shortenButton = document.querySelector('[data-js="btn-shorten"]');
 const responseField = document.querySelector('[data-js="get-url"]');
 
 // AJAX functions
+// USING XHR (XML Http Request)
+// const shortenUrl = () => {
+//   const urlToShorten = inputField.value;
+//   const data = JSON.stringify({destination:urlToShorten});
+//   const xhr = new XMLHttpRequest();
+//   xhr.responseType = 'json';
+//   xhr.onreadystatechange = () => {
+//     if(xhr.readyState === XMLHttpRequest.DONE) {
+//       renderResponse(xhr.response);
+//     }
+//   };
+//   xhr.open('POST', url);
+//   xhr.setRequestHeader('Content-type', 'application/json');
+//   xhr.setRequestHeader('apikey', apiKey);
+//   xhr.send(data);
+// }
+
+// USING FETCH()
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination:urlToShorten});
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+  const data = JSON.stringify({destination: urlToShorten})
+  
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
     }
-  };
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.setRequestHeader('apikey', apiKey);
-  xhr.send(data);
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then((jsonResponse) => {
+    renderResponse(jsonResponse);
+  });
 }
 
 
